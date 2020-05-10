cb885e8eb0bbf09d6acb32d9ecd767cfd6281271
tommelo <neilton@commerce-connector.com>
2020-05-10 10:29:12 +0200

    Added log adapter tests

---
+++ tests/unit/logger/adapters/SQSLogAdapterTests.js
@@ -0,0 +1,56 @@
+const { expect } = require("chai"),
+      { SQSLogAdapter } = require("../../../../lib/logger");
+
+describe("SQSLogAdapter", () => {
+
+  describe("#adapt()", () => {
+
+    it("should adapt sqs attributes", () => {
+      const event = {
+        "Records": [
+          {
+            "messageId": "1234567890",
+            "receiptHandle": "abcde",
+            "body": "{}",
+            "attributes": {
+              "ApproximateReceiveCount": "1",
+              "SentTimestamp": "1568708487072",
+              "SenderId": "abcde",
+              "ApproximateFirstReceiveTimestamp": "1568708487078"
+            },
+            "messageAttributes": {},
+            "md5OfBody": "abcd",
+            "eventSource": "aws:sqs",
+            "eventSourceARN": "arn:aws:sqs:eu-central-1:12345:any-queue-name",
+            "awsRegion": "eu-central-1"
+          }
+        ]
+      }
+
+      const { Records: [record] } = event;
+      const expected = {
+        level: null,
+        name: null,
+        payload: null,
+        messageId: record.messageId,
+        approximateReceiveCount: record.attributes.ApproximateReceiveCount,
+        sentTimestamp: record.attributes.SentTimestamp,
+        senderId: record.attributes.SenderId,
+        approximateFirstReceiveTimestamp: record.attributes.ApproximateFirstReceiveTimestamp,
+        messageAttributes: record.messageAttributes,
+        md5OfBody: record.md5OfBody,
+        eventSource: record.eventSource,
+        eventSourceARN: record.eventSourceARN,
+        awsRegion: record.awsRegion,
+        time: null
+      };
+
+      const body = new SQSLogAdapter().adapt(event);
+
+      expect(expected).to.deep.include(body);
+
+    })
+
+  })
+
+})
