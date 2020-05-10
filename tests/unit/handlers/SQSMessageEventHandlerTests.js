b1960782749ad843bbe8a46e53a6e0cc0ae83b23
tommelo <neilton@commerce-connector.com>
2020-05-10 11:00:22 +0200

    Added sqs message event handler tests

---
+++ tests/unit/handlers/SQSMessageEventHandlerTests.js
@@ -0,0 +1,45 @@
+const sinon = require("sinon"),
+      { expect } = require("chai"),
+      { SQSMessageEventHandler } = require("../../../lib/handlers");
+
+describe("SQSMessageEventHandler", () => {
+
+  beforeEach(() => { sinon.stub(console, "log") })
+
+  afterEach(() => { sinon.restore() })
+
+  describe("#handleEvent()", () => {
+
+    it("should execute use case", async() => {
+      const executeFake = sinon.fake(() => {});
+      const handler = new SQSMessageEventHandler({ execute: executeFake });
+
+      const event = {
+        "Records": [
+          {
+            "messageId": "1234567890",
+            "receiptHandle": "abcde",
+            "body": "{\"failOnBlacklistedEmail\":true,\"client\":\"my-client-service\",\"emailType\":\"RAW_TEXT\",\"from\":\"any@email.com\",\"to\":[\"any@email.com\"],\"replyTo\":[\"any@email\"],\"subject\":\"title\",\"body\":\"content\"}",
+            "attributes": {
+              "ApproximateReceiveCount": "1",
+              "SentTimestamp": "1568708487072",
+              "SenderId": "abcde",
+              "ApproximateFirstReceiveTimestamp": "1568708487078"
+            },
+            "messageAttributes": {},
+            "md5OfBody": "abcd",
+            "eventSource": "aws:sqs",
+            "eventSourceARN": "arn:aws:sqs:eu-central-1:12345:any-queue-name",
+            "awsRegion": "eu-central-1"
+          }
+        ]
+      }
+
+      await handler.handleEvent(event);
+
+      expect(true).to.equal(executeFake.calledOnce);
+    })
+
+  })
+
+})
